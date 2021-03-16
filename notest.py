dfee1f1e8fa089c6d4a6f4d5235f5b320276ef10
Redwane HADJI <redwanecr7@gmail.com>
2021-03-16 17:02:05 +0100

    First commit

---
+++ notest.py
@@ -0,0 +1,12 @@
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def parse(self, command):
+        return Action("add", command[2:])    
+
+
+class Action:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
